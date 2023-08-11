aca9de1b068942ad94c2d98dc79872f22ec4ff5e
LGlightflow <2285434325@qq.com>
2023-08-12 03:18:11 +0800

    exp1~3

--- main.cpp
+++ main.cpp
@@ -5,7 +5,10 @@
 #include<algorithm>
 using namespace std;
 
-
+/**
+ * @description: 输出vector中内容，以逗号分隔，方便在python中绘图
+ * @return {*}
+ */
 template <typename T>
 void vector_show(vector<T> const& arr){
     cout << "[";
@@ -16,6 +19,12 @@ void vector_show(vector<T> const& arr){
     cout << endl;
 }
 
+
+
+/**
+ * @description: 创建正弦波
+ * @return {*}
+ */
 template<typename T1, typename T2 ,typename T3>
 vector<double>  create_sin_arr(T1 amp,T2 freq,T3 phase,int sample_rate,int time_ms){
     static_assert(is_arithmetic<T1>::value || is_arithmetic<T2>::value ||
@@ -50,6 +59,14 @@ vector<double>  create_sin_arr(T1 amp,T2 freq,T3 phase,int sample_rate,int time_
 }
 
 
+/**
+ * @description: 生成正弦波，每个周期的点的数值是相同的
+ * @param {T1} amp
+ * @param {T2} freq
+ * @param {int} sample_rate
+ * @param {int} time_ms
+ * @return {*}
+ */
 template<typename T1, typename T2>
 vector<double>  create_sin_arr2(T1 amp,T2 freq,int sample_rate,int time_ms){
     
@@ -88,7 +105,10 @@ vector<double>  create_sin_arr2(T1 amp,T2 freq,int sample_rate,int time_ms){
 
 
 
-
+/**
+ * @description: 计算均值和方差等
+ * @return {*}
+ */
 template <typename T>
 void MV_cal(vector<T> const& arr){
     double sum = std::accumulate(begin(arr),end(arr),0.0);
@@ -105,18 +125,132 @@ void MV_cal(vector<T> const& arr){
 
     cout << "总和：" << sum << endl;
     cout << "均值：" << mean << endl;
-    cout << "方差：" << accum << endl;
+    cout << "方差：" << accum/(arr.size() - 1)  << endl;
     cout << "标准差：" << stdev << endl;
+}
+
+/**
+ * @description: 每一次循环计算一次均值和方差，以此模拟运行数据时(数据可以持续增加)的均值方差模型的计算
+ * @param {vector<T>} const
+ * @return {*}
+ */
+template <typename T>
+void MV_cal_running(vector<T> const& arr){
+
+
+    double sum = 0, mean, variance = 0, stdev = 0, sum_sqrt = 0;
+    for (int i = 0; i < arr.size();i++){
+        
+
+        sum = arr[i] + sum;
+        mean = sum / (i + 1);
+        sum_sqrt = sum_sqrt + arr[i] * arr[i];
+
+        variance = (sum_sqrt - sum * sum / (i + 1)) / i;
+        stdev = sqrt(variance);
+        cout << "总和：" << sum << endl;
+        cout << "均值：" << mean << endl;
+        cout << "方差：" << variance << endl;
+        cout << "标准差：" << stdev << endl<<endl;
+    }
+
+}
+
+
+/**
+ * @description: 计算直方图（似乎有内存泄漏问题）
+ * @param {vector<T>} const
+ * @param {double} acc 如果vector类型为小数，则将vector中的数*acc再取整
+ * @return {*}
+ */
+template <typename T>
+vector<int> binned_hist_cal(vector<T> const& arr,double acc){
+    T maxValue = *max_element(arr.begin(),arr.end());
+    double size = maxValue * acc;
+
+    while(size <= 0){
+        size = size * 10;
+        acc = acc * 10;
+    }
+
+    vector<int> hist(static_cast<int>(size)+1, 0);
+    for_each(arr.begin(),arr.end(), [&](const double d) {
+        int bin_num = static_cast<int>(d * acc);
+        hist[bin_num] ++;
+    });
+    
+    return hist;
+}
+
+template <typename T1,typename T2>
+vector<double> convolution_input_side(vector<T1> X,vector<T2> H){
+    vector<double> Y(X.size()+H.size(), 0);
+    for(int i = 0; i < X.size(); i++){ 
+        for(int j = 0; j < H.size(); j++){
+            Y[i + j] += X[i] * H[j];
+        }
+    }
+    return Y;
+}
+
+template <typename T1,typename T2>
+vector<double> convolution_output_side(vector<T1> X,vector<T2> H){
+    vector<double> Y(X.size()+H.size(), 0);
+    for(int i = 0; i < Y.size(); i++){ 
+        for(int j = 0; j < H.size(); j++){ 
+            if(i - j < 0) 
+                continue;
+            if(i - j >= X.size() ) 
+                continue;
+            Y[i] += X[i - j] * H[j];
+        }
+    }
+    return Y;
 }
 
 
 int main(){
-
-    vector<double> x;
+    vector<double> x,x_part;
     x = create_sin_arr(1,220, 0,4410,2000);
-    vector_show(x);
+
+    // vector<double>::const_iterator begin = x.begin() ; 
+    // vector<double>::const_iterator end = x.begin() + 1000;
+    // x_part.assign(begin, end);
+
+    // vector_show(x);
     // MV_cal(x);
 
+    /********************exp0.创建正弦波数据*************************/
+    // x = create_sin_arr(1,220, 0,4410,2000);
+    // vector_show(x);
+    /********************exp0.创建正弦波数据*************************/
+
+
+    /********************exp1.计算均值、方差*************************/
+    // std::vector<int> ids = []() {std::vector<int> v; v.resize(100); for (int i = 0; i < 100; ++i) v[i] = i; return v; }();
+    // MV_cal_running(ids);
+    // MV_cal(ids);
+    // vector_show(ids);
+    /********************exp1.计算均值、方差*************************/
+
+
+    /********************exp2.计算直方图*************************/
+    // x = create_sin_arr(1,220, 0,4410,2000);
+    // vector<int> hist = binned_hist_cal(x, 100);
+    // vector_show(hist);
+    /********************exp2.计算直方图*************************/
+
+
+
+    /********************exp3.卷积*************************/
+    vector<double> X={0, -1, -1.3, 2, 1.4, 1.4, 0.6, 0, -0.6};
+    vector<double> H={1, -0.5, -0.3, -0.2};
+    vector<double> Y;
+    Y = convolution_input_side(X, H);
+    vector_show(Y);
+    Y = convolution_output_side(X, H);
+    vector_show(Y);
+    /********************exp3.卷积*************************/
     system("pause");
     return 0;
 }
