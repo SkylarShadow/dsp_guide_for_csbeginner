0c917c78b70601d1c223f902bd4400473f8b6891
SKYlarShadow <851428179@qq.com>
2023-08-26 09:51:40 +0800

    清除无用信息

--- draw_func.py
+++ draw_func.py
@@ -1,11 +1,12 @@
 '''
-Author: LGlightflow 2285434325@qq.com
+Author: SKYlarShadow 851428179@qq.com
 Date: 2023-08-03 20:03:05
-LastEditors: LGlightflow 2285434325@qq.com
-LastEditTime: 2023-08-20 02:20:06
+LastEditors: SKYlarShadow 851428179@qq.com
+LastEditTime: 2023-08-26 09:50:41
 FilePath: \vscode\dsp_guide\draw_func.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
+
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.fftpack import fft,ifft
