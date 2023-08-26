3218a231f9ad59357d7edf4a0be2d7e79463c262
SKYlarShadow <851428179@qq.com>
2023-08-26 10:54:39 +0800

    clean

--- draw_func.py
+++ draw_func.py
@@ -1,11 +1,4 @@
-'''
-Author: SKYlarShadow 851428179@qq.com
-Date: 2023-08-03 20:03:05
-LastEditors: SKYlarShadow 851428179@qq.com
-LastEditTime: 2023-08-26 09:50:41
-FilePath: \vscode\dsp_guide\draw_func.py
-Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
-'''
+
 
 import matplotlib.pyplot as plt
 import numpy as np
